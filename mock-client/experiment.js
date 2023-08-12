465eaa9abf45c94df8d7c8173d69b090c5fdb08e
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2023-08-11 20:18:59 -0700

    Started writing experiment code. Brain is cheese, I will figure this out later because right now I will only do more harm than good

--- mock-client/experiment.js
+++ mock-client/experiment.js
@@ -10,6 +10,178 @@ const og = "e-srv"+process.env.EDGE_ID;
 const TIMER = 5000;
 const E_URL = "http://e-srv:3000";
 
-const EXP_CLEANED_DATA = true;
+const INIT_TIME = 7000;
 
-// Re-send email to Zitlaly?
+const EXP = {
+    CLEANED_DATA: true,
+    EDGE_SERVERS: 5,
+    FILES_PER_EDGE: 4,
+    FILES_SYNC_EDGE: 2,
+    MAC_START: 3,
+    BLOCK_START: 1,
+}
+
+const EDGE_TO_FILE_RANGE = {
+    'A': 0,
+    'B':1,
+    'C':2,
+    'D':3,
+    'E':4
+}
+
+const EDGE_TO_MAC_FILES = {
+    'A': ['MAC000003.csv','MAC000004.csv','MAC000005.csv','MAC000006.csv'],
+    'B': ['MAC000007.csv','MAC000008.csv','MAC000009.csv','MAC000010.csv'],
+    'C': ['MAC000011.csv','MAC000012.csv','MAC000013.csv','MAC000014.csv'],
+    'D': ['MAC000015.csv','MAC000016.csv','MAC000017.csv','MAC000018.csv'],
+    'E': ['MAC000019.csv','MAC000020.csv','MAC000021.csv','MAC000022.csv']
+}
+
+const EDGE_TO_BLOCK_FILES = {
+    'A': ['block_1.csv', 'block_2.csv', 'block_3.csv', 'block_4.csv'],
+    'B': ['block_5.csv', 'block_6.csv', 'block_7.csv', 'block_8.csv'],
+    'C': ['block_9.csv', 'block_10.csv', 'block_11.csv', 'block_12.csv'],
+    'D': ['block_13.csv', 'block_14.csv', 'block_15.csv', 'block_16.csv'],
+    'E': ['block_17.csv', 'block_18.csv', 'block_19.csv', 'block_20.csv'],
+}
+
+// Can determine a range of files to draw from, at the moment, my brain is cheese
+// so I'm just oging to hard code verything
+//const MAC_FILE_RANGE = (EXP.MAC_START, (EDGE_TO_FILE_RANGE[process.env.LOCAL_GROUP]*4) + 4)
+
+
+const EDGE_TO_FILES = EXP.CLEANED_DATA ? EDGE_TO_MAC_FILES : EDGE_TO_BLOCK_FILES;
+
+
+    EDGE_TO_FILES.forEach((file, i) => {
+        if (i%2===0) { // Even files go to cloud
+            setTimeout(()=> call_cloud_write_endpoint_on_edge(file), INIT_TIME);
+        }
+        else { // Odd files are local
+            setTimeout(()=> call_local_write_endpoint_on_edge(file), INIT_TIME);
+        }
+    })
+
+
+if (process.env.LOCAL_GROUP === 'A') {
+    // Group A's u1 will be performing all queries
+
+}
+
+
+
+
+
+function call_local_write_endpoint_on_edge(filename) {
+
+    const file = fs.createReadStream(`mock-client-data/${filename}`);
+    const formData = new FormData();
+
+    formData.append('filename', filename);
+    formData.append('file', file); //Automatically deals with size
+
+    f.HOFetch(`http://${og}:${EXPRESS_PORT}/local-write`, 
+    {
+        method: 'POST',
+        headers: {
+        },
+        body: formData
+    },
+    (res) => {
+        console.log(res.message);
+    });
+}
+
+
+function call_cloud_write_endpoint_on_edge(filename) {
+
+    const file = fs.createReadStream(`mock-client-data/${filename}`);
+
+    const formData = new FormData();
+
+    formData.append('filename', filename);
+    formData.append('file', file); //Automatically deals with size
+
+    f.HOFetch(`http://${og}:${EXPRESS_PORT}/cloud-write`, 
+    {
+        method: 'POST',
+        headers: {
+        },
+        body: formData
+    },
+    (res) => {
+        console.log(res.message);
+    });
+}
+
+// [ 
+//     {
+//         "owner":null,
+//         "bucket": "B",
+//         "from_files":["test/MAC000004.csv"]
+//     },
+//     {
+//         "owner":"A",
+//          "from_files":["MAC000003.csv"]
+//     },
+//     {
+//         "owner":"C",
+//          "from_files":["MAC000005.csv"]
+//     }
+// ]
+
+// Make sure that undefined is working properly as inf!!!
+
+// Dynamically generate query_components and pass them in
+function Q1_MAC(query_components) {
+    console.log('Reading local and cloud data with hybrid-query, testing policy engine authentication');
+
+    fetch(`http://c-srv:${EXPRESS_PORT}/read-query`, {
+        method: 'POST',
+        headers: {
+            "accept": "application/json",
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            "user": process.env.USERNAME,
+            "select_fields": ['tstp', 'energy(kWh/hh)', 'LCLid'],
+            "query_components": query_components,
+            "where": [
+                { field: 'tstp', range: ['2013-01-01 00:00:00', '2013-02-01 00:00:00']},
+                { field: 'energy(kWh/hh)', range: ['0.120', undefined]}
+            ]
+            
+        })
+    })
+    .then(res=>res.json() )
+    .then((response)=>{
+        console.log(JSON.stringify(response.query_results));
+    })
+    .catch((error)=>console.error("Error",error));
+}
+
+function Q2_MAC(query_components) {
+    console.log('Reading local and cloud data with hybrid-query, testing policy engine authentication');
+
+    fetch(`http://c-srv:${EXPRESS_PORT}/read-query`, {
+        method: 'POST',
+        headers: {
+            "accept": "application/json",
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            "user": process.env.USERNAME,
+            "select_fields": ['tstp', 'energy(kWh/hh)', 'LCLid'],
+            "query_components": query_components,
+            "where": [
+                { field: 'energy(kWh/hh)', range: ['0.120', undefined]}
+            ]
+            
+        })
+    })
+    .then(res=>res.json() )
+    .then((response)=>{
+        console.log(JSON.stringify(response.query_results));
+    })
+    .catch((error)=>console.error("Error",error));
+}
